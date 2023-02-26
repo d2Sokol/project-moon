d17401d2cceac01d73815720b85c25d522fe776c
d2Sokol <d2.sokolowski@gmail.com>
2023-02-26 19:47:20 +0100

    New Project

---
+++ Source/PROJECT_MOON/PROJECT_MOON.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PROJECT_MOON.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PROJECT_MOON, "PROJECT_MOON" );
---
+++ Source/PROJECT_MOON/PROJECT_MOONGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PROJECT_MOONGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MOON_API APROJECT_MOONGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
